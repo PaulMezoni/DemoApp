b05d2fb03425df56f8d44f6b0681879d5f1c8a47
pavelmezin <paulmezoni@gmail.com>
2025-10-31 22:02:32 +0300

    initial

---
+++ src/main/java/org/demoapp/controller/Controller.java
@@ -0,0 +1,16 @@
+package org.demoapp.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
